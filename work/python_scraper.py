10e17f74d1f71f7de74f69a4bc1091c816b09dc9
rs-kkit0 <38030070+rs-kkit0@users.noreply.github.com>
2020-08-19 23:30:14 +0900

    python_scraper修正

--- work/python_scraper.py
+++ work/python_scraper.py
@@ -14,7 +14,7 @@ def main():
     books = scrape(html, url)
     save('books.csv', books)
 
-def fetch(url: string) -> str:
+def fetch(url: str) -> str:
     """
     urlのWebページを取得する.
     WebページのエンコーディングはContent-Typeヘッダーから取得する
@@ -22,3 +22,42 @@ def fetch(url: string) -> str:
     return: str型のHTML
     """
     
+    r = requests.get(url)
+    return r.text
+
+def scrape(html: str, base_url: str) -> List[dict]:
+    """
+    HTMLから正規表現で書籍の情報を抜き出す.
+    """
+
+    books = []
+    html = lxml.html.fromstring(html)
+    html.make_links_absolute(base_url)
+
+    for a in html.cssselect('#listBook > li > a[itemprop="url"]'):
+        url = a.get('href')
+
+        p = a.cssselect('p[itemprop="name"]')[0]
+        title = p.text_content()
+
+        books.append({'url': url, 'title': title})
+
+    return books
+
+def save(file_path: str, books: List[dict]):
+    """
+    csvに保存する
+    """
+
+    with open(file_path, 'w', newline='') as f:
+        writer = csv.DictWriter(f, ['url', 'title'])
+        writer.writeheader()
+        writer.writerows(books)
+    
+# pythonコマンドで実行された場合にmainを呼ぶ
+# モジュールとして他のファイルから呼び出されたときにmainが実行されないようにするための
+# Pythonの一般的なイディオム
+
+if __name__ == '__main__':
+    main()
+
